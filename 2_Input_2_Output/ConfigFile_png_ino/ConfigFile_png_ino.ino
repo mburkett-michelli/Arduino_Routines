e75a523187d7c47e1003622f4b3f404d04c01342
mburkett-michelli <mburkett@michelli.com>
2026-04-14 13:02:40 -0600

    Added files to repo

---
+++ 2_Input_2_Output/ConfigFile_png_ino/ConfigFile_png_ino.ino
@@ -0,0 +1,107 @@
+
+/*
+  DigitalReadSerial
+  Reads a digital input on pin 2, prints the result to the serial monitor
+
+  This example code is in the public domain.
+*/
+#include <PLDuino.h>
+using namespace PLDuino;
+
+// digital pin 37 is used for the gate interlock. Give it a name:
+// digital pin 38 is used for the pushbutton.  Give it a name:
+
+int GateInterLock = 37;
+int GateInterlockState = 0;
+boolean GateLocked = false;
+String GateString = "";
+
+int PushButton = 36;
+int PushButtonState = 0;
+boolean ButtonPushed = false;
+String PushButtonString = "";
+
+String Output = "";
+
+unsigned long GateStartTime = 0;
+const long interval = 1000;
+unsigned long PushButtonStartTime = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // Serial3 is for the USB connection, Serial is used for the local serial port.
+  // initialize serial communication at 9600 bits per second:
+  Serial3.begin(9600);
+  
+  // make the Gate Interlock pin an input:
+  pinMode(GateInterLock, INPUT_PULLUP);
+  pinMode(PushButton, INPUT_PULLUP);
+  
+}
+
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+
+  String content = "";
+  char character;
+
+
+  while (Serial3.available()) {
+    character = Serial3.read();
+    content.concat(character);
+  }
+
+    unsigned long CurrentTime = millis();
+    // read the input pins only when prompted:
+    int GateInterlockState = digitalRead(GateInterLock);
+    if (GateInterlockState == 0)
+    {
+      // If the gate is locked
+      GateStartTime = CurrentTime; 
+      GateLocked = true;
+    }
+  
+    PushButtonState = digitalRead(PushButton);
+
+    if (PushButtonState == 0)
+    {
+      PushButtonStartTime = CurrentTime;
+      ButtonPushed = true;
+    }
+  
+
+  if (content.indexOf("T") >= 0)
+  {
+    
+    if (CurrentTime - GateStartTime <= interval)
+    {
+      //Serial3.println("Y");
+      GateString="1";
+      
+    }
+    else
+    {
+      //Serial3.println("N");
+      GateString="0";
+      GateLocked=false;
+    }
+    if (CurrentTime - PushButtonStartTime <= interval)
+    {
+      PushButtonString="1";
+      
+    }
+    else
+    {
+      PushButtonString="0";
+      ButtonPushed = false;
+    }
+  
+  Output=GateString+PushButtonString;
+  Serial3.println(Output);
+   
+  }
+  
+}
---
+++ Arduino_TempSensor/_2560TempSensor.ino/_2560TempSensor.ino.ino
@@ -0,0 +1,65 @@
+/*************************************************** 
+  This is a library example for the MLX90614 Temp Sensor
+
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1748
+  ----> https://www.adafruit.com/products/1749
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+String CommandString = "f#1= ";
+String TempString = "";
+char EndOfLine[]={13,10};
+String OutputString = "";
+
+void setup() {
+  Serial.begin(9600);
+    
+  mlx.begin();  
+}
+
+void loop() {
+
+  TempString = String(mlx.readObjectTempF());
+  OutputString = CommandString + TempString;
+  /**
+  An Example of how to send out a string one character at a time should it be necessary
+  
+  OutputString = OutputString+'\r'+'\n';
+
+  int StrLength = OutputString.length()+1;
+  int i;
+  char charBuf[StrLength];
+  OutputString.toCharArray(charBuf,StrLength);
+  Serial.println(StrLength);
+  for (i=0; i<StrLength; i++){
+
+    Serial.write(OutputString[i]);
+    
+  }
+  **/
+  Serial.println(OutputString);
+  /**
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+ 
+  Serial.println();
+   **/
+   
+  delay(500);
+}
